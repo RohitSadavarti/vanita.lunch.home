9e24d105f20d4fc2c11dfbf3a00d0e4fb1c437fa
Rohit Sadavarti <rsadavarti2@gmail.com>
2025-09-28 01:27:07 +0530

    Update build.sh

--- OrderMaster/build.sh
+++ OrderMaster/build.sh
@@ -5,5 +5,4 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
-python manage.py migrate OrderMaster 0002_add_payment_id --fake
 python manage.py migrate
