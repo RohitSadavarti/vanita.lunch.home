65b053c90d399372ce22145088441a6e40294b9b
Rohit Sadavarti <rsadavarti2@gmail.com>
2025-09-27 16:40:44 +0530

    Update wsgi.py

--- OrderMaster/vanita_lunch/wsgi.py
+++ OrderMaster/vanita_lunch/wsgi.py
@@ -8,9 +8,18 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
+import traceback
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vanita_lunch.settings')
 
-application = get_wsgi_application()
+try:
+    application = get_wsgi_application()
+except Exception:
+    # Print the full traceback to the console to see the real error
+    print("!!! DJANGO FAILED TO START - TRACEBACK BELOW !!!", file=sys.stderr)
+    traceback.print_exc(file=sys.stderr)
+    # Exit with a non-zero status code to indicate failure
+    sys.exit(1)
