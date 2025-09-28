c0cf371593662c6ece1c9204993251bced42ddd3
Rohit Sadavarti <rsadavarti2@gmail.com>
2025-09-28 15:20:05 +0530

    Update build.sh

--- OrderMaster/build.sh
+++ OrderMaster/build.sh
@@ -9,6 +9,5 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 # Create and apply database migrations for the OrderMaster app
-# This is the most reliable way to ensure migrations are handled.
 python manage.py makemigrations OrderMaster
 python manage.py migrate
