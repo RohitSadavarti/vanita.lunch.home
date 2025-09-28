acbeafb658d79a63757aa25278fd71fc950bff11
Rohit Sadavarti <rsadavarti2@gmail.com>
2025-09-28 17:02:16 +0530

    Update forms.py

--- OrderMaster/OrderMaster/forms.py
+++ OrderMaster/OrderMaster/forms.py
@@ -6,7 +6,6 @@ from .models import MenuItem
 class MenuItemForm(forms.ModelForm):
     class Meta:
         model = MenuItem
-        # The 'image' field has been removed from this list to match your model
         fields = [
             'item_name',
             'description',
