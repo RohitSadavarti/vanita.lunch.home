cd09b0d0942e4c74a70f42d85795bf5ccccc7ce2
Rohit Sadavarti <rsadavarti2@gmail.com>
2025-09-30 00:46:20 +0530

    Update build.sh

--- OrderMaster/build.sh
+++ OrderMaster/build.sh
@@ -1,15 +1,7 @@
 #!/usr/bin/env bash
-# Exit immediately if a command exits with a non-zero status.
 set -o errexit
 
-# Install all dependencies from requirements.txt
 pip install -r requirements.txt
-
-# Run collectstatic to gather all static files into STATIC_ROOT
 python manage.py collectstatic --no-input
-
-# Create new database migrations based on model changes
-python manage.py makemigrations OrderMaster
-
-# Apply all database migrations
+python manage.py makemigrations
 python manage.py migrate
