9f768debff65913db91ecc00d0582a9bb7912e2c
Rohit Sadavarti <rsadavarti2@gmail.com>
2025-09-29 08:52:40 +0530

    Update build.sh

--- OrderMaster/build.sh
+++ OrderMaster/build.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
+# Exit on error
 set -o errexit
 
+# Install dependencies
 pip install -r requirements.txt
+
+# Collect static files
 python manage.py collectstatic --no-input
+
+# Create and apply database migrations
 python manage.py makemigrations OrderMaster
 python manage.py migrate
