d697e9dd4c02a7d5539a75274df027bb98a53098
Rohit Sadavarti <rsadavarti2@gmail.com>
2025-09-29 06:19:53 +0530

    Update urls.py

--- OrderMaster/vanita_lunch/urls.py
+++ OrderMaster/vanita_lunch/urls.py
@@ -1,29 +1,12 @@
-"""
-URL configuration for vanita_lunch project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns # Add this import
+# --- Import the new view from its new location ---
+from .views import firebase_messaging_sw
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # This URL pattern for the service worker is now correct
+    path('firebase-messaging-sw.js', firebase_messaging_sw, name='firebase-messaging-sw'),
+    # This includes all the URLs from your OrderMaster app
     path('', include('OrderMaster.urls')),
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += staticfiles_urlpatterns() # Add this line
