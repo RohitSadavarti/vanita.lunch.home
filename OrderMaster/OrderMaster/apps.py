4b9c28bb1592cb065dbc3bcebb0322206361ad5b
Rohit Sadavarti <rsadavarti2@gmail.com>
2025-09-29 15:27:08 +0530

    Update apps.py

--- OrderMaster/OrderMaster/apps.py
+++ OrderMaster/OrderMaster/apps.py
@@ -2,4 +2,5 @@ from django.apps import AppConfig
 
 class OrdermasterConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'OrderMaster.OrderMaster'  # Changed from 'OrderMaster'
+    # Correct the name to just the app's name
+    name = 'OrderMaster'
