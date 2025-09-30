b155892737a63a8e9ca82e74eec9f2fbc9eb30db
Rohit Sadavarti <rsadavarti2@gmail.com>
2025-10-01 01:22:36 +0530

    Create App.js

---
+++ OrderMaster/frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import AnalyticsDashboard from './AnalyticsDashboard';
+import './App.css'; // We will add styles here
+
+function App() {
+  return (
+    <div className="App">
+      <AnalyticsDashboard />
+    </div>
+  );
+}
+
+export default App;
