efb24aa9223414619ab4391fbae6b84e2221faac
Rohit Sadavarti <rsadavarti2@gmail.com>
2025-10-01 16:22:47 +0530

    Update urls.py

--- OrderMaster/OrderMaster/urls.py
+++ OrderMaster/OrderMaster/urls.py
@@ -1,5 +1,7 @@
-from django.urls import path
+from django.urls import path, include, re_path
 from . import views
+from OrderMaster.scripts.analytics_views import urlpatterns as analytics_urlpatterns
+
 urlpatterns = [
     # Admin URLs
     path('', views.login_view, name='login'),
@@ -8,13 +10,15 @@ urlpatterns = [
     path('orders/', views.order_management_view, name='order_management'),
     path('menu/', views.menu_management_view, name='menu_management'),
     path('menu/delete/<int:item_id>/', views.delete_menu_item_view, name='delete_menu_item'),
-    path('analytics/', views.analytics_view, name='analytics'),
-     path('analytics/', include((analytics_urlpatterns, 'analytics'))),
+    re_path(r'^analytics/$', views.analytics_view, name='analytics'),
+    path('analytics/', include(analytics_urlpatterns)),
+    
     path('settings/', views.settings_view, name='settings'),
     path('api/subscribe-topic/', views.subscribe_to_topic, name='subscribe_topic'),
     path('api/analytics/', views.analytics_api_view, name='analytics_api'),
     path('api/handle-order-action/', views.handle_order_action, name='handle_order_action'),
-    # API URLs
+    
+    # Other API URLs
     path('api/menu-item/<int:item_id>/', views.api_menu_item_detail, name='api_menu_item_detail'),
     path('api/update-order-status/', views.update_order_status, name='update_order_status'),
     path('api/get_orders/', views.get_orders_api, name='get_orders_api'),
@@ -22,12 +26,5 @@ urlpatterns = [
     path('api/place-order/', views.api_place_order, name='api_place_order'),
 
     # Customer-facing URL
-   path('customer-order/', views.customer_order_view, name='customer_home'),
+    path('customer-order/', views.customer_order_view, name='customer_home'),
 ]
-
-
-
-
-
-
-
