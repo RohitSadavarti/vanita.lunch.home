ff61d86960ed59e58bd56f8936e282811fa5dbcd
Rohit Sadavarti <rsadavarti2@gmail.com>
2025-10-02 12:10:09 +0530

    Update main.js

--- OrderMaster/static/js/main.js
+++ OrderMaster/static/js/main.js
@@ -7,19 +7,19 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // --- UTILITY: Get CSRF Token ---
     const getCookie = (name) => {
-        let cookieValue = null;
-        if (document.cookie && document.cookie !== '') {
-            const cookies = document.cookie.split(';');
-            for (let i = 0; i < cookies.length; i++) {
-                const cookie = cookies[i].trim();
-                if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                    break;
-                }
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
             }
         }
-        return cookieValue;
-    };
+    }
+    return cookieValue;
+};
 //--------------------------------------------------------------------------------------------------------------------------
 
 function showNewOrderPopup(orderData) {
@@ -259,3 +259,4 @@ async function handleOrderAction(orderId, action, modalInstance) {
         });
     }
 });
+
