bf559fbe7247fa140390f0492d5e167351e77315
Rohit Sadavarti <rsadavarti2@gmail.com>
2025-10-02 10:52:13 +0530

    Update urls.py

--- OrderMaster/vanita_lunch/urls.py
+++ OrderMaster/vanita_lunch/urls.py
@@ -1,41 +1,37 @@
 # OrderMaster/vanita_lunch/urls.py
 """
 URL configuration for vanita_lunch project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
-# --- THIS IS THE CORRECTED PART ---
-# We now correctly import the analytics URLs from the scripts folder.
-from OrderMaster.scripts import analytics_views
-# ------------------------------------
-
+from django.views.generic import TemplateView # Import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    # --- AND WE ADD THE CORRECT PATH HERE ---
-    # This tells Django to look inside analytics_views.py for any URL starting with 'analytics/'
-    path('analytics/', include(analytics_views.urlpatterns)),
-    # ----------------------------------------
-    
-    # This includes all the main app URLs (dashboard, orders, etc.)
+    # --- THIS IS THE CRITICAL FIX ---
+    # This line correctly includes all the URLs from your OrderMaster app,
+    # such as the dashboard, orders, menu, and all the APIs.
     path('', include('OrderMaster.urls')),
+    # ------------------------------------
+
+    # This handles the analytics page separately
+    path('analytics/', include('OrderMaster.scripts.analytics_views.urlpatterns')),
+    
+    # --- FIX FOR FIREBASE SERVICE WORKER ---
+    # This serves the firebase-messaging-sw.js file from the root directory,
+    # which is required by Firebase for background notifications to work.
+    path(
+        "firebase-messaging-sw.js",
+        TemplateView.as_view(
+            template_name="firebase-messaging-sw.js",
+            content_type="application/javascript",
+        ),
+        name="firebase-messaging-sw.js",
+    ),
+    # -----------------------------------------
 ]
 
 # This is important for serving images during development
