0644a7eadf75ee6a07c6b9612b1e3eb1a83e4ebf
Rohit Sadavarti <rsadavarti2@gmail.com>
2025-10-02 18:09:17 +0530

    Update urls.py

--- OrderMaster/vanita_lunch/urls.py
+++ OrderMaster/vanita_lunch/urls.py
@@ -1,44 +1,31 @@
+# vanita_lunch/urls.py
+
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.http import HttpResponse
-from django.views.decorators.cache import never_cache
+from django.template.loader import render_to_string
 
-@never_cache
-def firebase_sw(request):
-    js_code = """
-importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBnYYq_K3TL9MxyKaCNPkB8SRqAIucF0rI",
-  authDomain: "vanita-lunch-home.firebaseapp.com",
-  projectId: "vanita-lunch-home",
-  storageBucket: "vanita-lunch-home.firebasestorage.app",
-  messagingSenderId: "86193565341",
-  appId: "1:86193565341:web:b9c234bda59b37ee366e74"
-};
-
-firebase.initializeApp(firebaseConfig);
-const messaging = firebase.messaging();
-
-messaging.onBackgroundMessage((payload) => {
-  console.log('Background message received:', payload);
-  const title = payload.notification.title;
-  const options = {
-    body: payload.notification.body,
-    icon: '/static/favicon.ico'
-  };
-  self.registration.showNotification(title, options);
-});
-"""
-    return HttpResponse(js_code, content_type='application/javascript')
+# This view dynamically serves the Firebase Service Worker JS file
+def firebase_messaging_sw(request):
+    """
+    Renders the firebase-messaging-sw.js file with the correct context.
+    This ensures your Firebase config is correctly injected.
+    """
+    return HttpResponse(
+        render_to_string('firebase-messaging-sw.js'),
+        content_type='application/javascript'
+    )
 
 urlpatterns = [
-    path('firebase-messaging-sw.js', firebase_sw, name='firebase-sw'),
+    # URL for the service worker
+    path('firebase-messaging-sw.js', firebase_messaging_sw, name='firebase-messaging-sw'),
+    
+    # Include your app's URLs
     path('', include('OrderMaster.urls')),
 ]
 
+# This is for serving media files during development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
