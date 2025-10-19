63439e52ffa69a506847b0ee85244b955415654a
Rohit Sadavarti <rsadavarti2@gmail.com>
2025-10-19 23:36:47 +0530

    Update build.sh

--- OrderMaster/build.sh
+++ OrderMaster/build.sh
@@ -5,9 +5,9 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate
-
 echo "Building React frontend..."
 cd OrderMaster/frontend
 npm install
 npm run build
 cd ../.. 
+
