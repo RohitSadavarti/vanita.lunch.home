af10f818598c0a544adc203a5adac3bf28345509
Rohit Sadavarti <rsadavarti2@gmail.com>
2025-10-19 23:37:09 +0530

    Update build.sh

--- OrderMaster/build.sh
+++ OrderMaster/build.sh
@@ -3,11 +3,18 @@ set -o errexit
 
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
-python manage.py makemigrations
+# Remove potentially conflicting old migration history for the OrderMaster app
+rm -rf OrderMaster/OrderMaster/migrations/
+mkdir -p OrderMaster/OrderMaster/migrations/
+touch OrderMaster/OrderMaster/migrations/__init__.py
+
+# Now create and apply fresh migrations
+python manage.py makemigrations OrderMaster
 python manage.py migrate
 echo "Building React frontend..."
 cd OrderMaster/frontend
 npm install
 npm run build
 cd ../.. 
+
 
