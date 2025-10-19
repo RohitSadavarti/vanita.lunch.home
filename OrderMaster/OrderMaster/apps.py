275f5b21e31644ab459fbea7c85daf3232f86ca7
Rohit Sadavarti <rsadavarti2@gmail.com>
2025-10-19 21:30:58 +0530

    Delete OrderMaster directory

--- OrderMaster/OrderMaster/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-class OrdermasterConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    # Correct the name to just the app's name
-    name = 'OrderMaster'
