c6ffbc04731587ba132836beca32139fc958b6d8
Rohit Sadavarti <rsadavarti2@gmail.com>
2025-11-17 01:31:14 +0530

    Update app.py

--- vanitalunchhome/app.py
+++ vanitalunchhome/app.py
@@ -145,6 +145,7 @@ def place_order():
                     'id': str(new_order_db_id),
                     'order_id': order_id,
                     'customer_name': name,
+                    'customer_phone': mobile,
                     'total_price': str(total_price),
                     'items': json.dumps(validated_items),
                     'order_source': 'customer'
@@ -187,4 +188,5 @@ if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     debug_mode = os.environ.get('FLASK_ENV') == 'development'
     app.run(host='0.0.0.0', port=port, debug=debug_mode)
+
 
